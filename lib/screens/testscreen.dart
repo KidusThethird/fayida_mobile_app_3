58b825ab640585feff3c78a1603a876d7d2a73dd
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-08-14 14:18:24 +0300

    update: package fetch page tested

--- lib/screens/testscreen.dart
+++ lib/screens/testscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../models/packagelist.dart';
 import '../services/packageList_services.dart';
 
 class PackagesListScreen extends StatelessWidget {
@@ -13,11 +14,36 @@ class PackagesListScreen extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () {
-                PackageListServices().getAllPackages();
+                print(PackageListServices().getAllPackages());
               },
               icon: Icon(Icons.refresh))
         ],
       ),
+      body: FutureBuilder(
+          future: PackageListServices().getAllPackages(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error fetching package data"),
+              );
+            }
+            if (snapshot.hasData) {
+              var data = snapshot.data as List<Package>;
+              return ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(data[index].imgUrl![0]),
+                    ),
+                    title: Text("${data[index].packageName}"),
+                  );
+                },
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 }
