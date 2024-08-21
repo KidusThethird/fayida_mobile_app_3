7c3fa65b6df765519484602c6df430b02dc9139c
kidus Tesfaye <kidus.z3rd@gmail.com>
2024-08-22 02:41:36 +0300

    update: notification page created

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:online_course/utils/data.dart';
-import 'package:online_course/widgets/chat_item.dart';
-import 'package:online_course/widgets/custom_textfield.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -11,6 +10,48 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<dynamic> notifications = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotifications();
+  }
+
+  Future<void> fetchNotifications() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? cookieString = prefs.getString('cookies');
+
+    if (cookieString != null) {
+      final Dio dio = Dio();
+      dio.options.headers['Cookie'] = cookieString;
+
+      try {
+        final response =
+            await dio.get('https://api.fayidaacademy.com/notifications');
+
+        if (response.statusCode == 200) {
+          setState(() {
+            notifications = response.data; // Assuming data is the list
+            isLoading = false;
+          });
+        } else {
+          // Handle error (status code not 200)
+          setState(() {
+            isLoading = false;
+          });
+        }
+      } catch (e) {
+        // Handle exceptions
+        print("Error fetching notifications: $e");
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -18,47 +59,68 @@ class _ChatPageState extends State<ChatPage> {
       child: Column(
         children: [
           _buildHeader(),
-          _buildChats(),
+          _buildNotifications(),
         ],
       ),
     );
   }
 
-  _buildHeader() {
+  Widget _buildHeader() {
     return Padding(
       padding: EdgeInsets.fromLTRB(0, 60, 0, 5),
-      child: Column(
-        children: [
-          Text(
-            "Chat",
-            style: TextStyle(
-              fontSize: 28,
-              color: Colors.black87,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 15),
-          CustomTextBox(
-            hint: "Search",
-            prefix: Icon(
-              Icons.search,
-              color: Colors.grey,
-            ),
-          ),
-        ],
+      child: Text(
+        "Notifications",
+        style: TextStyle(
+          fontSize: 28,
+          color: Colors.black87,
+          fontWeight: FontWeight.w600,
+        ),
       ),
     );
   }
 
-  _buildChats() {
-    return ListView(
-      padding: EdgeInsets.only(top: 10),
-      shrinkWrap: true,
-      children: List.generate(
-        chats.length,
-        (index) => ChatItem(
-          chats[index],
-        ),
+  Widget _buildNotifications() {
+    if (isLoading) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: ListView.builder(
+        padding: EdgeInsets.all(10),
+        shrinkWrap: true,
+        itemCount: notifications.length,
+        itemBuilder: (context, index) {
+          final notification = notifications[index];
+
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 2,
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: ListTile(
+              contentPadding: EdgeInsets.all(10),
+              title: Text(
+                notification['notiHead'],
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 18, 54, 19)),
+              ),
+              subtitle: Text(notification['notiFull']),
+            ),
+          );
+        },
       ),
     );
   }
